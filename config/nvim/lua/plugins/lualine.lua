73f943eb4d8fe247f1665fffa5790172ae40439a
takepro14 <takepro100pm@gmail.com>
2024-05-30 23:41:02 +0900

    fix(lualine): re-enabled tab key

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -95,23 +95,6 @@ return {
         lualine_y = {},
         lualine_z = {}
       },
-      tabline = {
-        lualine_a = {
-          {
-            'buffers',
-            mode = 4,
-            icons_enabled = true,
-            show_filename_only = true,
-            hide_filename_extensions = false
-          }
-        },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { 'tabs' }
-      },
-      extensions = {}
     }
   end
 }
