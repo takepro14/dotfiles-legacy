7cd48f877d97ae43a0f91880fdb61b682ad741e8
takepro14 <takepro100pm@gmail.com>
2024-06-01 15:30:01 +0900

    feat(comment): keymap for multiple lines

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -4,12 +4,9 @@ return {
   dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
   opts = {
     enable_autocmd = false,
-    toggler = {
-      line = "<Leader>/",
-    },
-    extra = {
-      eol = "<Leader>a",
-    },
+    toggler = { line = "<Leader>/" },  -- for single line
+    opleader = { line = "<Leader>/" }, -- for multiple lines
+    extra = { eol = "<Leader>a" },
     pre_hook = function()
       require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
     end,
