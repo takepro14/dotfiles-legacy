e30932e19989e1d2ba3052ef583faac701c9b514
takepro14 <takepro100pm@gmail.com>
2024-06-02 23:03:36 +0900

    feat(init): transparent background

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,6 +3,8 @@ vim.g.mapleader = " "
 require("core.options")
 require("core.lazy")
 vim.cmd.colorscheme("dracula")
+vim.cmd('highlight Normal guibg=none')
+vim.cmd('highlight NonText guibg=none')
 vim.o.cmdheight = 1
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
