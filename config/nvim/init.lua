e1e019030ac6edfd0f8c70c9ca23f783d9070b21
takepro14 <takepro100pm@gmail.com>
2024-06-08 16:15:05 +0900

    refactor(vim): cmdheight

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,7 +5,6 @@ require("core.lazy")
 vim.cmd.colorscheme("dracula")
 vim.cmd('highlight Normal guibg=none')
 vim.cmd('highlight NonText guibg=none')
-vim.o.cmdheight = 1
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
