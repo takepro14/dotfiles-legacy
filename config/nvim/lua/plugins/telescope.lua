0678aa03a53196a10d4034c28d3ebf0c61888671
takepro14 <takepro100pm@gmail.com>
2024-06-13 13:58:37 +0900

    feat(telescope): add option to find_files (include hidden files)

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -7,10 +7,10 @@ return {
     version = "^1.0.0"
   },
   keys = {
-    { "<leader>ff", "<cmd>Telescope find_files<CR>", {} },
-    { "<leader>fg", "<cmd>Telescope live_grep<CR>",  {} },
-    { "<leader>fb", "<cmd>Telescope buffers<CR>",    {} },
-    { "<leader>fh", "<cmd>Telescope help_tags<CR>",  {} }
+    { "<leader>ff", "<cmd>Telescope find_files hidden=true<CR>", {} },
+    { "<leader>fg", "<cmd>Telescope live_grep<CR>",              {} },
+    { "<leader>fb", "<cmd>Telescope buffers<CR>",                {} },
+    { "<leader>fh", "<cmd>Telescope help_tags<CR>",              {} }
   },
   config = function()
     local telescope = require("telescope")
