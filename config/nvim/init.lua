79f0cda5ba66e9a4c2489c00bd2be93d82062c2e
takepro14 <takepro100pm@gmail.com>
2024-06-14 11:57:49 +0900

    feat(init): modify highlight color for select text

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,7 +5,7 @@ require("core.lazy")
 vim.cmd.colorscheme("dracula")
 vim.cmd('highlight Normal guibg=none')
 vim.cmd('highlight NonText guibg=none')
-vim.cmd('highlight Visual guibg=#f1fa8c guifg=NONE')
+vim.cmd('highlight Visual guibg=#6272A4 guifg=NONE')
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
