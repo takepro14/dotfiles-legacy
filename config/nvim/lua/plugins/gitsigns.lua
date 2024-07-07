6d12986d7823f5768d5df25de5aa0e6558f08bd6
takepro14 <takepro100pm@gmail.com>
2024-07-07 17:49:19 +0900

    feat(gitsigns): activate and add keybind

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,21 @@
+-- plugins.lua
 return {
   "lewis6991/gitsigns.nvim",
-  config = true,
+  config = function()
+    require('gitsigns').setup {
+      current_line_blame = true,
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map('n', '<leader>gb', gs.toggle_current_line_blame)
+        map('n', '<leader>gd', gs.diffthis)
+      end,
+    }
+  end
 }
