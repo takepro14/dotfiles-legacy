c917724a85b18b64f08dbf65bce072895c136dea
takepro14 <takepro100pm@gmail.com>
2024-11-14 23:58:21 +0900

    feat(salesforce): improve functions for soql and sf

--- zsh/salesforce.zsh
+++ zsh/salesforce.zsh
@@ -1,23 +1,28 @@
 # Salesforce functions
 
 soql() {
-  local query=$1 org=${2:-prod}
-  sf data query --query $query --target-org $org
+  local query=$1 org=$2
+  if [[ $query == *"*"* ]]; then
+    local obj=$(echo "$query" | sed -n 's/.*from \([a-zA-Z0-9_]*\).*/\1/p')
+    local all_columns=$(sf force:schema:sobject:describe -s $obj -o $org | jq -r '.fields[].name' | tr '\n' ',' | sed 's/,$//')
+    query=$(echo "$query" | sed "s/\*/$all_columns/")
+  fi
+  echo "----- \nSOQL: $query\n-----"
+  sf data query --query $query -o $org
 }
 
-sfprefix() {
-  local org=${1:-prod}
-  sf data query --query "SELECT QualifiedApiName, Label, KeyPrefix FROM EntityDefinition" --target-org $org
-}
-
-sfcol() {
-  local obj=$1 org=${2:-prod}
-  sf force:schema:sobject:describe --target-org $org -s $obj | jq -r '.fields[] | [.label, .name] | @sh' | awk 'BEGIN {print "fields_array=("} {print "  (" $0 ")"} END {print ")"}'
+sfprefixes() {
+  local org=$1
+  sf data query --query 'SELECT QualifiedApiName, Label, KeyPrefix FROM EntityDefinition' -o $org
 }
 
 sfmeta() {
-  local obj=$1 org=${2:-prod}
+  local obj=$1 org=$2
   sf force:schema:sobject:describe -s $obj -o $org
 }
 
+sfcols() {
+  local obj=$1 org=$2
+  sf force:schema:sobject:describe -o $org -s $obj | jq -r '.fields[] | [.label, .name] | @sh' | awk 'BEGIN {print "fields_array=("} {print "  (" $0 ")"} END {print ")"}'
+}
 
