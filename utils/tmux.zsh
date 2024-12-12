7c85f0962215325eab763580e0b688d472367416
takepro14 <takepro100pm@gmail.com>
2024-12-12 09:41:31 +0900

    feat(tmux): improve functions and add alias

--- utils/tmux.zsh
+++ utils/tmux.zsh
@@ -1,23 +1,27 @@
+# ===================================
 # Tmux utilities
+# ===================================
 
-tmset() {
+alias tmls='tmux ls'
+alias tmk='tmux kill-session'
+alias tma='tmux attach'
+
+tmux!() {
   tmux kill-session
   local session_name='🧛‍♂️ takepro14'
   tmux new-session -d -s "$session_name" -n '.' -c "${HOME}/.dotfiles"
   tmux new-window -t "$session_name" -n 'Dropbox' -c "${HOME}/Dropbox"
   tmux new-window -t "$session_name" -n 'dev' -c "${HOME}/dev"
-  tmat
-}
-
-tmat() {
+  tmux select-window -t "$session_name"
   local session=$(tmux ls -F "#{session_name}" | fzf --prompt="Session: ")
-  [[ -n $session ]] && tmux a -t $session
+  [[ -n $session ]] && tmux a -t "$session" \; select-window -t "$session:1"
 }
 
-tmide() {
-  tmux split-window -h
-  tmux resize-pane -R 30
+ide() {
   tmux split-window -v
+  tmux resize-pane -D 10
+  tmux split-window -h
   tmux select-pane -t 0
   nvim
 }
+
