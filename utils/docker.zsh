5f73bb38698b52623df2ec45b5a6cf7fc8b56afa
takepro14 <takepro100pm@gmail.com>
2024-12-12 09:46:01 +0900

    feat(docker): alias to function

--- utils/docker.zsh
+++ utils/docker.zsh
@@ -1,13 +1,41 @@
+# ===================================
 # Docker utilities
+# ===================================
 
-alias -g DI='docker images | fzf | awk "{print \$3}"'
-alias -g DC='docker ps | fzf | awk "{print \$1}"'
-alias dsh='docker run --rm -it $(DI) sh'
-alias dat='docker attach $(DC)'
-alias drm='docker rm -vf $(docker ps -aq)'
-alias drmi='docker rmi $(docker images -f "dangling=true" -q)'
-alias drmv='docker volume rm $(docker volume ls -qf dangling=true)'
-alias dcbuild='docker-compose build --no-cache'
-alias dcup='docker-compose up'
-alias dcdown='docker-compose down'
-alias dcrestart='docker-compose restart'
+# --- Docker Commands ---
+alias dp='docker ps'
+alias di='docker images'
+
+dsh() {
+  docker run --rm -it $(docker images | fzf | awk "{print \$3}") sh
+}
+
+dat() {
+  docker attach $(docker ps | fzf | awk "{print \$1}")
+}
+
+drm() {
+  docker rm -vf $(docker ps -aq)
+}
+
+drmi() {
+  docker rmi $(docker images -f "dangling=true" -q)
+}
+
+drmv() {
+  docker volume rm $(docker volume ls -qf dangling=true)
+}
+
+dnginx() {
+  read 'mode?Shell or Server [sh/sv]: '
+  [[ "$mode" == "sh" ]] && docker run --rm -it nginx sh
+  [[ "$mode" == "sv" ]] && docker run --rm -d -p 8080:80 nginx && open 'http://localhost:8080'
+}
+
+# --- Docker Compose Commands ---
+alias dcb='docker compose build'
+alias dcu='docker compose up'
+alias dcd='docker compose down'
+alias dcr='docker compose restart'
+
+
