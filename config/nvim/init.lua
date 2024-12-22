df0a576d6863985b48fc14517c1a81bcd869a901
takepro14 <takepro100pm@gmail.com>
2024-12-23 07:53:24 +0900

    refactor(vim): highlight settings split into new file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,47 +2,7 @@ vim.loader.enable()
 vim.g.mapleader = ' '
 require('core.options')
 require('core.lazy')
-vim.cmd.colorscheme('dracula')
-
-vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
-vim.cmd('highlight NonText guibg=NONE ctermbg=NONE')
-vim.cmd('highlight Visual guibg=#6272A4 guifg=NONE')
-vim.cmd('highlight NvimTreeNormal guibg=NONE ctermbg=NONE')
-vim.cmd('highlight BufferLineFill guibg=NONE ctermbg=NONE')
-vim.cmd('highlight BufferLineBackground guibg=NONE ctermbg=NONE')
-vim.cmd('highlight BufferLineBufferSelected guibg=NONE ctermbg=NONE')
-vim.cmd('highlight BufferLineBufferVisible guibg=NONE ctermbg=NONE')
-vim.cmd('highlight TabLineFill guibg=NONE ctermbg=NONE')
-vim.cmd('highlight TabLine guibg=NONE ctermbg=NONE')
-vim.cmd('highlight TabLineSel guibg=NONE ctermbg=NONE')
-vim.cmd('highlight StatusLine guibg=NONE ctermbg=NONE')
-vim.cmd('highlight StatusLineNC guibg=NONE ctermbg=NONE')
-vim.cmd('highlight VertSplit guibg=NONE ctermbg=NONE')
-vim.cmd('highlight LineNr guibg=NONE ctermbg=NONE')
-vim.cmd('highlight CursorLineNr guibg=NONE ctermbg=NONE')
-vim.cmd('highlight SignColumn guibg=NONE ctermbg=NONE')
-vim.cmd('highlight EndOfBuffer guibg=NONE ctermbg=NONE')
-vim.cmd('highlight TelescopeNormal guibg=NONE ctermbg=NONE')
-vim.cmd('highlight TelescopeBorder guibg=NONE ctermbg=NONE')
-vim.cmd('highlight TelescopePromptNormal guibg=NONE ctermbg=NONE')
-vim.cmd('highlight TelescopePromptBorder guibg=NONE ctermbg=NONE')
-vim.cmd('highlight TelescopeResultsNormal guibg=NONE ctermbg=NONE')
-vim.cmd('highlight TelescopeResultsBorder guibg=NONE ctermbg=NONE')
-vim.cmd('highlight TelescopePreviewNormal guibg=NONE ctermbg=NONE')
-vim.cmd('highlight TelescopePreviewBorder guibg=NONE ctermbg=NONE')
-vim.cmd('highlight CmpNormal guibg=NONE ctermbg=NONE')
-vim.cmd('highlight CmpPmenu guibg=NONE ctermbg=NONE')
-vim.cmd('highlight CmpPmenuSel guibg=NONE ctermbg=NONE')
-vim.cmd('highlight CmpPmenuThumb guibg=NONE ctermbg=NONE')
-vim.cmd('highlight CmpDocumentation guibg=NONE ctermbg=NONE')
-vim.cmd('highlight CmpDocumentationBorder guibg=NONE ctermbg=NONE')
-
-vim.cmd([[
-  highlight DiffAdd    guibg=#3a5640 guifg=NONE ctermbg=DarkGreen ctermfg=NONE
-  highlight DiffChange guibg=#4e437a guifg=NONE ctermbg=DarkBlue ctermfg=NONE
-  highlight DiffDelete guibg=#603d3d guifg=NONE ctermbg=DarkRed ctermfg=NONE
-  highlight DiffText   guibg=#6c5076 guifg=NONE ctermbg=DarkMagenta ctermfg=NONE
-]])
+require('core.colors')
 
 vim.api.nvim_create_autocmd('User', {
   pattern = 'VeryLazy',
---
+++ config/nvim/lua/core/colors.lua
@@ -0,0 +1,40 @@
+vim.cmd.colorscheme('dracula')
+
+vim.cmd([[
+  highlight Normal                    guibg=NONE ctermbg=NONE
+  highlight NonText                   guibg=NONE ctermbg=NONE
+  highlight Visual                    guibg=#6272A4 guifg=NONE
+  highlight NvimTreeNormal            guibg=NONE ctermbg=NONE
+  highlight BufferLineFill            guibg=NONE ctermbg=NONE
+  highlight BufferLineBackground      guibg=NONE ctermbg=NONE
+  highlight BufferLineBufferSelected  guibg=NONE ctermbg=NONE
+  highlight BufferLineBufferVisible   guibg=NONE ctermbg=NONE
+  highlight TabLineFill               guibg=NONE ctermbg=NONE
+  highlight TabLine                   guibg=NONE ctermbg=NONE
+  highlight TabLineSel                guibg=NONE ctermbg=NONE
+  highlight StatusLine                guibg=NONE ctermbg=NONE
+  highlight StatusLineNC              guibg=NONE ctermbg=NONE
+  highlight VertSplit                 guibg=NONE ctermbg=NONE
+  highlight LineNr                    guibg=NONE ctermbg=NONE
+  highlight CursorLineNr              guibg=NONE ctermbg=NONE
+  highlight SignColumn                guibg=NONE ctermbg=NONE
+  highlight EndOfBuffer               guibg=NONE ctermbg=NONE
+  highlight TelescopeNormal           guibg=NONE ctermbg=NONE
+  highlight TelescopeBorder           guibg=NONE ctermbg=NONE
+  highlight TelescopePromptNormal     guibg=NONE ctermbg=NONE
+  highlight TelescopePromptBorder     guibg=NONE ctermbg=NONE
+  highlight TelescopeResultsNormal    guibg=NONE ctermbg=NONE
+  highlight TelescopeResultsBorder    guibg=NONE ctermbg=NONE
+  highlight TelescopePreviewNormal    guibg=NONE ctermbg=NONE
+  highlight TelescopePreviewBorder    guibg=NONE ctermbg=NONE
+  highlight CmpNormal                 guibg=NONE ctermbg=NONE
+  highlight CmpPmenu                  guibg=NONE ctermbg=NONE
+  highlight CmpPmenuSel               guibg=NONE ctermbg=NONE
+  highlight CmpPmenuThumb             guibg=NONE ctermbg=NONE
+  highlight CmpDocumentation          guibg=NONE ctermbg=NONE
+  highlight CmpDocumentationBorder    guibg=NONE ctermbg=NONE
+  highlight DiffAdd                   guibg=#3a5640 guifg=NONE ctermbg=DarkGreen ctermfg=NONE
+  highlight DiffChange                guibg=#4e437a guifg=NONE ctermbg=DarkBlue ctermfg=NONE
+  highlight DiffDelete                guibg=#603d3d guifg=NONE ctermbg=DarkRed ctermfg=NONE
+  highlight DiffText                  guibg=#6c5076 guifg=NONE ctermbg=DarkMagenta ctermfg=NONE
+]])
