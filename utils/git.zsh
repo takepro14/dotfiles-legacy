1ef5970b646da4811c0a4396edee5511010b6782
takepro14 <takepro100pm@gmail.com>
2024-12-29 00:41:41 +0900

    feat(git): aliases and functions to utility zsh file

---
+++ utils/git.zsh
@@ -0,0 +1,80 @@
+# Git utilities
+
+local GIT_USERNAME=$(git config user.name)
+
+# --- Git (Local Repo) ---
+alias g='git'
+alias ga='git add'
+alias gd='git diff'
+alias gp='git push'
+alias gl='git log'
+alias gcm='git commit'
+alias gst='git status'
+alias gsh='git stash'
+alias gsw='git switch'
+alias gbr='git branch'
+alias gre='git restore'
+alias gus='git reset HEAD' # unstage
+alias guc='git reset --soft HEAD^' # uncommit
+
+gco() {
+  git branch | fzf | xargs git switch
+}
+
+grmb() {
+  git branch | fzf | xargs git branch -D
+}
+
+ghs() {
+  git log --no-merges --date=short \
+    --pretty='format:%C(yellow)%h %C(green)%cd %C(blue)%an%C(red)%d %C(reset)%s'
+}
+
+ggr() {
+  git log --graph -10 --branches --remotes --tags  \
+    --format=format:'%Cgreen%h %Creset• %<(75,trunc)%s (%cN, %ar) %Cred%d' \
+    --date-order
+}
+
+gdf() {
+  git diff --color=always $(git diff --name-only | fzf)
+}
+
+gaf() {
+  git add $(git status -s | sed 's/^?? / U /' | fzf -m | awk '{print $NF}')
+}
+
+gloc() {
+  read 'start_date?Start Date (default: 2024-01-01): '
+  read 'end_date?End Date (default: 2024-12-31): '
+  start_date=${start_date:-2024-01-01}
+  end_date=${end_date:-2024-12-31}
+  git log --numstat --pretty='%H' --author="$GIT_USERNAME" \
+    --since="$start_date" --until="$end_date" --no-merges | \
+    awk 'NF==3 {add+=$1; del+=$2} END {printf("%d (+%d, -%d)\n", add+del, add, del)}'
+}
+
+repo() {
+  cd $(ghq list -p | fzf)
+}
+
+
+# --- GitHub (Remote Repo) ---
+# ex. ghrepo | ghrepo create | ghrepo clone
+ghrepo() {
+  if [[ -z "$1" ]]; then
+    gh repo view --web
+  elif [[ "$1" == "create" ]]; then
+    local repo_name=$(basename "$(pwd)")
+    gh repo create "$repo_name" --private --source=. --push
+  elif [[ "$1" == "clone" ]]; then
+    local username="$GIT_USERNAME"
+    local reponame=$(gh repo list "$username" | fzf | awk '{print $1}')
+    [[ -n "$reponame" ]] && ghq get "$reponame"
+  fi
+}
+
+ghpr() {
+  gh pr view --web
+}
+
