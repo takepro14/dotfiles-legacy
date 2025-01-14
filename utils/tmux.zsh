c92c6e4508914deb20ea656941980289f95f9b41
takepro14 <takepro100pm@gmail.com>
2025-01-14 11:44:19 +0900

    fix(tmux): correct home directory notation

--- utils/tmux.zsh
+++ utils/tmux.zsh
@@ -10,7 +10,7 @@ tmux!() {
   tmux kill-server 2>/dev/null
   local config="$HOME/.dotfiles/utils/config/tmux.json"
   local name=$(jq -r '.name' "$config")
-  local windows=($(jq -r '.windows[]' "$config"))
+  local windows=($(jq -r '.windows[]' "$config" | sed "s|\$HOME|$HOME|g"))
   tmux new-session -d -s "$name" -n "$(basename "${windows[1]}")" -c "${windows[1]}"
   for window in "${windows[@]:1}"; do
     tmux new-window -t "$name" -n "$(basename "$window")" -c "$window"
