8b0db8704c3e62e1a7d009855f57c065cfcf295c
takepro14 <takepro100pm@gmail.com>
2025-01-14 14:38:56 +0900

    refactor(tmux): modify config file structure to hash in array

--- utils/tmux.zsh
+++ utils/tmux.zsh
@@ -10,13 +10,25 @@ tmux!() {
   tmux kill-server 2>/dev/null
   local config="$HOME/.dotfiles/utils/config/tmux.json"
   local name=$(jq -r '.name' "$config")
-  local windows=($(jq -r '.windows[]' "$config" | sed "s|\$HOME|$HOME|g"))
-  tmux new-session -d -s "$name" -n "$(basename "${windows[1]}")" -c "${windows[1]}"
+  local windows=($(jq -c '.windows[]' "$config" | sed "s|\$HOME|$HOME|g"))
+  tmux new-session -d -s "$name" \
+    -n $(_window_title "${windows[1]}") \
+    -c $(_window_path "${windows[1]}")
   for window in "${windows[@]:1}"; do
-    tmux new-window -t "$name" -n "$(basename "$window")" -c "$window"
+    tmux new-window -t "$name" \
+      -n $(_window_title "$window") \
+      -c $(_window_path "$window")
   done
   tmux select-window -t "$name:1"
   tmux attach-session -t "$name"
+}
+
+_window_title() {
+  echo "$1" | jq -r '.title'
+}
+
+_window_path() {
+  echo "$1" | jq -r '.path'
 }
 
 ide() {
