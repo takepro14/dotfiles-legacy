d178c60d7670a56d6076fcec24e310868c9e9473
takepro14 <takepro100pm@gmail.com>
2025-01-20 10:16:19 +0900

    feat(salesforce): improve functions for checking argument

--- utils/salesforce.zsh
+++ utils/salesforce.zsh
@@ -4,7 +4,7 @@
 
 soql() {
   local query="$1" org="$2"
-  [[ ! "$#" -eq 2 ]] && echo "Usage: soql <query> <org> [all|(json|csv)]" && return 1
+  [[ "$#" -eq 0 ]] && echo "Usage: soql <query> <org> [all|(json|csv)]" && return 1
   shift 2
   local extra_opts=("$@")
   if [[ $query == *"*"* ]]; then
@@ -21,19 +21,19 @@ soql() {
 
 sfpref() {
   local org="$1"
-  [[ ! "$#" -eq 1 ]] && echo "Usage: sfpref <org>" && return 1
+  [[ "$#" -eq 0 ]] && echo "Usage: sfpref <org>" && return 1
   sf data query -q 'SELECT QualifiedApiName, Label, KeyPrefix FROM EntityDefinition' -o $org
 }
 
 sfmeta() {
   local obj="$1" org="$2"
-  [[ ! "$#" -eq 2 ]] && echo "Usage: sfmeta <obj> <org>" && return 1
+  [[ "$#" -eq 0 ]] && echo "Usage: sfmeta <obj> <org>" && return 1
   sf sobject describe -s $obj -o $org
 }
 
 sfcol() {
   local obj="$1" org="$2"
-  [[ ! "$#" -eq 2 ]] && echo "Usage: sfcol <obj> <org>" && return 1
+  [[ "$#" -eq 0 ]] && echo "Usage: sfcol <obj> <org>" && return 1
   sf sobject describe -s $obj -o $org |\
     jq -r '.fields[] | [.label, .name] | @sh' |\
     awk 'BEGIN {print "fields=("} {print "  (" $0 ")"} END {print ")"}'
@@ -41,7 +41,7 @@ sfcol() {
 
 sfacc() {
   local userid="$1" recordid="$2" org="$3"
-  [[ ! "$#" -eq 3 ]] && echo "Usage: sfacc <userid> <recordid> <org>" && return 1
+  [[ "$#" -eq 0 ]] && echo "Usage: sfacc <userid> <recordid> <org>" && return 1
   sf data query -q "SELECT RecordId, HasReadAccess FROM UserRecordAccess WHERE UserId = '$userid' AND RecordId = '$recordid'" -o $org
 }
 
