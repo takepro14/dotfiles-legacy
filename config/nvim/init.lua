70a641db93003141b3cf4fab313249b7468c5c7e
takepro14 <takepro100pm@gmail.com>
2025-03-01 15:32:47 +0900

    feat(vim): Add commands to copy path

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -8,6 +8,7 @@ vim.api.nvim_create_autocmd('User', {
   pattern = 'VeryLazy',
   callback = function()
     require('core.autocmds')
+    require('core.user_commands')
     require('core.keymaps')
     require('core.lsp')
   end,
