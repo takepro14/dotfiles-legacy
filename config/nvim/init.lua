70d74338c1cad90bba592d600179da438ace00da
takepro14 <takepro100pm@gmail.com>
2025-03-02 22:55:22 +0900

    feat(*): Change color theme from Dracula to GitHub

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,7 +2,6 @@ vim.loader.enable()
 vim.g.mapleader = ' '
 require('core.options')
 require('core.lazy')
-require('core.colors')
 
 vim.api.nvim_create_autocmd('User', {
   pattern = 'VeryLazy',
---
+++ config/nvim/lua/plugins/github-theme.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false,
+    config = function()
+      require('github-theme').setup({})
+      vim.cmd('colorscheme github_light')
+    end,
+  },
+}
