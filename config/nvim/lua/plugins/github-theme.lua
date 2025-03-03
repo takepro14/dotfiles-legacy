ae20e1a97f8c339074d991c0f51eff92c9dd3f2e
takepro14 <takepro100pm@gmail.com>
2025-03-03 22:54:31 +0900

    style(*): change editor cursor color to #ffffff

--- config/nvim/lua/plugins/github-theme.lua
+++ config/nvim/lua/plugins/github-theme.lua
@@ -3,14 +3,7 @@ return {
     'projekt0n/github-nvim-theme',
     lazy = false,
     config = function()
-      require('github-theme').setup({
-        groups = {
-          all = {
-            Cursor = { fg = '#ffffff', bg = '#3578e5' },
-          },
-        },
-      })
-      vim.o.guicursor = 'n-v-c:block-Cursor/lCursor,i:ver25-Cursor'
+      require('github-theme').setup({})
       vim.cmd('colorscheme github_light')
     end,
   },
