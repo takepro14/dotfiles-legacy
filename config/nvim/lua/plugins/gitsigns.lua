46b792e76e986ecea20cfeebbe17b08691719af2
takepro14 <takepro100pm@gmail.com>
2025-03-06 00:02:04 +0900

    feat(vim): add key binding for preview_hunk in gitsigns and simplify code

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,10 @@
--- plugins.lua
 return {
   'lewis6991/gitsigns.nvim',
   lazy = true,
-  event = { 'CursorHold', 'CursorHoldI' },
+  event = { 'BufReadPre', 'BufNewFile' },
+  keys = {
+    { '<leader>gd', '<cmd>Gitsigns preview_hunk<CR>', desc = 'Preview Git Diff' },
+  },
   config = function()
     require('gitsigns').setup({
       current_line_blame = true,
@@ -10,18 +12,6 @@ return {
         delay = 500,
       },
       current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> · <summary> · <abbrev_sha>',
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        map('n', '<leader>gb', gs.toggle_current_line_blame)
-        map('n', '<leader>gd', gs.diffthis)
-      end,
     })
   end,
 }
