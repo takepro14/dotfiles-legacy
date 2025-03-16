2ac927085f34c94abe9cedbd90a9619588a3de29
takepro14 <takepro100pm@gmail.com>
2025-03-16 10:08:51 +0900

    Revert "feat(vim): add noice.nvim plugin"
    This reverts commit 8bf2cf6a68939629d41b205f782088704d36019c.
    
    After installing noice.nvim, the following issues arose:
    - The bottom part of Neovim gets cut off when using `ide` with `resize-pane -D` in tmux.
    - Minor messages (e.g., `:w` saves) appear too frequently, creating noise.
    - The command mode modal was difficult to adapt to.
    
    Reverting to restore the previous behavior and usability.

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -7,6 +7,9 @@ return {
   config = function()
     require("gitsigns").setup({
       current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 500,
+      },
       current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> · <summary> · <abbrev_sha>",
     })
   end,
