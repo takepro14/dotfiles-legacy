b1728d7cb0df2677d7a34394f60d7c414f805eba
takepro14 <takepro100pm@gmail.com>
2025-03-20 23:20:30 +0900

    feat(vim): remap <C-k> to <C-p> and <C-j> to <C-n> for Emacs-style nvim-cmp navigation

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -31,8 +31,8 @@ return {
         }),
       },
       mapping = {
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.abort(),
